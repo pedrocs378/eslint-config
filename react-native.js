13565fcd910a850b0108f12160da4971036b5af5
Pedro <pedrocs378@gmail.com>
2022-09-27 10:59:32 -0300

    feat: add react-native config and update libs

---
+++ react-native.js
@@ -0,0 +1,110 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaFeatures: {
+          jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module'
+      }
+    }
+  ],
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'prettier/prettier': ['error', {
+      printWidth: 80,
+      tabWidth: 2,
+      singleQuote: true,
+      trailingComma: 'all',
+      arrowParens: 'always',
+      semi: false,
+    }],
+    camelcase: 'off',
+    'no-unused-expressions': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'react/no-children-prop': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/promise-function-async': [
+      'error',
+      {
+        checkArrowFunctions: false
+      }
+    ],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: [
+          '.tsx'
+        ]
+      }
+    ],
+    '@typescript-eslint/no-use-before-define': 'warn',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '_'
+      }
+    ],
+    '@typescript-eslint/camelcase': 'off',
+    'import/no-duplicates': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never'
+      }
+    ]
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {}
+    }
+  }
+}
